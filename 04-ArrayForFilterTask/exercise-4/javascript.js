cd8abb7a47ead884037df133918af929905c34d2
FezileChelebiyeva <fezile.celebiyevaa@gmail,com>
2022-11-02 02:09:21 +0400

    javascript and c#

---
+++ 04-ArrayForFilterTask/exercise-4/javascript.js
@@ -0,0 +1,9 @@
+let sampleNews = "As software developers, the natural tendency is to start developing applications based on your own hands-on experience and knowledge right away. However, overtime issues in the application arise, adaptations to changes, and new features happen."
+let a = 0;
+for( let i=0; i < sampleNews.length; i++){
+     bosh = sampleNews[i];
+    if (bosh !== "." && bosh !== ",") {
+        continue
+    }
+    a++;
+} console.log(a)
